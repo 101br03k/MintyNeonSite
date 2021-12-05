33e651a2712ef591c1d6d4d56952cf0239f1cf93
A3 <59868436+101br03k@users.noreply.github.com>
2021-12-05 13:38:56 +0100

    Add files via upload

---
+++ js/console_message.js
@@ -0,0 +1,7 @@
+console.log(`%c
+----------------------------------------------------------
+-- welkom in the console of this website    --
+-- i dont know what you want to do here,    --
+-- but i think is not gonne be very special --
+---------------------------------------------------------
+`,"color: #000; background:#fedc00; font-family: arial");
--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-console.log("What do you want to do in here?");
-//document.getElementById("btncookies").onclick = function() {
-//    document.getElementByID("cookies").remove();
-//}
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY > 20){
+        $(".navbar").addClass("sticky");
+        $(".goTop").fadeIn();
+      }
+      else{
+        $(".navbar").removeClass("sticky");
+        $(".goTop").fadeOut();
+      }
+    });
+  
+    $(".goTop").click(function(){scroll(0,0)});
+  
+    $('.menu-toggler').click(function(){
+      $(this).toggleClass("active");
+      $(".navbar-menu").toggleClass("active");
+    });
+  
+    $(".works").magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery:{enabled:true}
+    });
+  });
+  
+  
